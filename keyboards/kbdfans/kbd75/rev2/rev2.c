ccca1919bd1f003894d6517eb4b1eec3649f4cd8
Henrik O. Skogmo <henrik@skogmo.me>
2019-10-17 01:59:51 +0100

    UK and Utility keymap layer

--- keyboards/kbdfans/kbd75/rev2/rev2.c
+++ keyboards/kbdfans/kbd75/rev2/rev2.c
@@ -2,10 +2,13 @@
 
 void led_set_kb(uint8_t usb_led) {
   // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+  // Inverse caps lock lighting. Turn led off when caps active.
   if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-    writePinLow(B2);
-  } else {
     writePinHigh(B2);
+    // writePinLow(B2);
+  } else {
+    writePinLow(B2);
+    // writePinHigh(B2);
   }
 
   led_set_user(usb_led);
